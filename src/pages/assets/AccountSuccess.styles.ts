407e388aab898463e12a15543745f2fb75211693
javadocq <han18264@naver.com>
2025-05-16 00:05:42 +0900

    feat: accountsuccess 페이지 제작

--- src/pages/assets/AccountSuccess.styles.ts
+++ src/pages/assets/AccountSuccess.styles.ts
@@ -4,4 +4,15 @@ export const AccountSuccessContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding-top: 247px;
+`;
+
+export const AccountSuccessLabel = styled.p`
+  color: ${(props) => props.theme.gray.gy6};
+  text-align: center;
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  letter-spacing: -0.44px;
 `;
--- src/pages/assets/AccountSuccess.tsx
+++ src/pages/assets/AccountSuccess.tsx
@@ -1,10 +1,15 @@
 import * as S from "./AccountSuccess.styles";
+import { ReactComponent as SuccessIcon } from "@/assets/icons/success.svg";
 
 export default function AccountSuccess() {
   return (
     <S.AccountSuccessContainer>
-      <h1>Account Success</h1>
-      <p>Your account has been successfully created!</p>
+      <SuccessIcon width={224} height={224} />
+      <S.AccountSuccessLabel>
+        계좌를 등록했어요
+        <br />
+        앞으로 해당 계좌로 용돈을 받아요
+      </S.AccountSuccessLabel>
     </S.AccountSuccessContainer>
   );
 }
