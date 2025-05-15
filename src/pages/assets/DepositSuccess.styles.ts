54683122a42778926cef64061d9cfa94d02553b2
javadocq <han18264@naver.com>
2025-05-16 01:07:12 +0900

    feat: depositsuccess 페이지 제작

--- src/pages/assets/DepositSuccess.styles.ts
+++ src/pages/assets/DepositSuccess.styles.ts
@@ -4,7 +4,4 @@ export const DepositSuccessContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100vh;
 `;
--- src/pages/assets/DepositSuccess.tsx
+++ src/pages/assets/DepositSuccess.tsx
@@ -1,5 +1,12 @@
+import { Success } from "@/components";
 import * as S from "./DepositSuccess.styles";
 
 export default function DepositSuccess() {
-  return <S.DepositSuccessContainer>DepositSuccess</S.DepositSuccessContainer>;
+  return (
+    <S.DepositSuccessContainer>
+      <Success
+        label={`입금 신청을 완료했어요 🎉\n챌린지 하고 다시 용돈 받아보아요`}
+      />
+    </S.DepositSuccessContainer>
+  );
 }
