3cfd066b8e90d8b5d9aed46a5527482e4ce631b5
javadocq <han18264@naver.com>
2025-05-15 21:35:30 +0900

    feat: account3 페이지 제작

--- src/pages/assets/Account3.tsx
+++ src/pages/assets/Account3.tsx
@@ -1,10 +1,25 @@
+import { TopNav } from "@/components";
 import * as S from "./Account3.styles";
+import Set from "@/components/set/Set";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Account3() {
+  const navigate = useNavigate();
+  const [amount, setAmount] = useState("");
+  const handleMove = () => {
+    navigate("/asset/depositsuccess");
+  };
+
   return (
     <S.Account3Container>
-      <h1>Account3</h1>
-      <p>This is the Account3 page.</p>
+      <TopNav />
+      <Set
+        label="얼마를 입금할까요?"
+        amount={amount}
+        setAmount={setAmount}
+        onClick={handleMove}
+      />
     </S.Account3Container>
   );
 }
