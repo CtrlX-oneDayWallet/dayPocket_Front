76b585f19515957a3e3cc6fdc2cfddb12d2ff074
javadocq <han18264@naver.com>
2025-05-24 17:36:28 +0900

    feat: 목표 금액 api 수정

---
+++ src/lib/axionsInstance.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+  headers: {
+    Authorization: process.env.REACT_APP_API_TOKEN,
+    "Content-Type": "application/json",
+  },
+  timeout: 5000,
+});
+
+export default axiosInstance;
--- src/pages/assets/Asset1.tsx
+++ src/pages/assets/Asset1.tsx
@@ -3,14 +3,22 @@ import * as S from "./Asset1.styles";
 import Set from "@/components/set/Set";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axiosInstance from "@/lib/axionsInstance";
 
 export default function SetPage() {
   const navigate = useNavigate();
   const [amount, setAmount] = useState("0");
 
-  const handleMove = () => {
-    navigate("/asset");
-  };
+  async function handleMove() {
+    try {
+      await axiosInstance.post("/dayPocket/receipt/target", amount);
+      navigate("/asset");
+    } catch (error) {
+      console.error("Error occurred while setting the amount:", error);
+      alert("금액 설정에 실패했습니다. 다시 시도해주세요.");
+      return;
+    }
+  }
   return (
     <S.SetPageContainer>
       <TopNav />
