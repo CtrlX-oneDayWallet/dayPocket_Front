dc9e24ba2772cb1f558390fa7d88fc7735d9e9d1
javadocq <han18264@naver.com>
2025-05-24 18:17:42 +0900

    feat: quiz list 불러오기

--- src/pages/quiz/Explanation.tsx
+++ src/pages/quiz/Explanation.tsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import * as S from "./Explanation.styles";
 import { Button } from "@/components";
 import { useNavigate } from "react-router-dom";
+import axiosInstance from "@/lib/axionsInstance";
 
 const questions = [
-  { id: 1, text: "신용카드를 잘 쓰면\n신용점수가 오른다.", correct: true },
-  { id: 2, text: "적금은 중간에 깨도\n이자가 똑같다.", correct: false },
-  { id: 3, text: "주식 계좌는 앱으로도\n만들 수 있다.", correct: true },
-  { id: 4, text: "비상금 대출은 무조건\n직장이 있어야 된다.", correct: false },
-  { id: 5, text: "연 5% 적금이면 매달\n5% 이자가 붙는다.", correct: false },
+  { id: 1, text: "신용카드를 잘 쓰면\n신용점수가 오른다.", answer: true },
+  { id: 2, text: "적금은 중간에 깨도\n이자가 똑같다.", answer: false },
+  { id: 3, text: "주식 계좌는 앱으로도\n만들 수 있다.", answer: true },
+  { id: 4, text: "비상금 대출은 무조건\n직장이 있어야 된다.", answer: false },
+  { id: 5, text: "연 5% 적금이면 매달\n5% 이자가 붙는다.", answer: false },
 ];
 
 export default function Explanation() {
@@ -20,6 +21,22 @@ export default function Explanation() {
   const date = new Date();
   const month = date.getMonth() + 1;
   const day = date.getDate();
+
+  useEffect(() => {
+    async function fetchAnswers() {
+      try {
+        const res = await axiosInstance("/quiz-list", {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        console.log(res.data);
+      } catch (error) {
+        console.error("Error fetching quiz:", error);
+      }
+    }
+    fetchAnswers();
+  }, []);
 
   const handleAnswer = (id: number, value: boolean) => {
     setAnswers((prev) =>
