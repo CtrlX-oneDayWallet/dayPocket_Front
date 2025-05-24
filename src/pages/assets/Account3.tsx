678c55dcde69906c88937d1f54f71591e1de59b1
javadocq <han18264@naver.com>
2025-05-24 19:07:36 +0900

    feat: 입금 api

--- src/pages/assets/Account3.tsx
+++ src/pages/assets/Account3.tsx
@@ -3,13 +3,26 @@ import * as S from "./Account3.styles";
 import Set from "@/components/set/Set";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axiosInstance from "@/lib/axionsInstance";
 
 export default function Account3() {
   const navigate = useNavigate();
   const [amount, setAmount] = useState("");
-  const handleMove = () => {
-    navigate("/asset/depositsuccess");
-  };
+
+  async function handleMove() {
+    try {
+      await axiosInstance.post("/point/withdraw", Number(amount), {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      navigate("/asset/depositsuccess");
+    } catch (error) {
+      console.error("Error occurred while setting the amount:", error);
+      navigate("/asset/depositfail");
+      return;
+    }
+  }
 
   return (
     <S.Account3Container>
